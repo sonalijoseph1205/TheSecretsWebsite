9450a2687d55fd9224aefa3da546a2feb2c5883b
sonalijoseph1205 <sonalijoseph1205@gmail.com>
2021-01-14 12:54:01 +0530

    bug fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(passport.initialize());//to initialize the passport package
 app.use(passport.session());//to use passport for dealing with the sessions
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-sonali:sonali1205@cluster1.mmsmd.mongodb.net/userDB",{useNewUrlParser:true, useUnifiedTopology:true});
 mongoose.set("useCreateIndex",true);
 
 const userSchema= new mongoose.Schema({
